097d66510bd3714803b7fb40c2e21e1bb9ea48c0
EnricoCiccolini <piroroma90@gmail.com>
2025-03-24 15:39:02 +0100

    fix: people fixed

--- people.js
+++ people.js
@@ -1,11 +1,10 @@
 const hobbies = require("./hobbies")
 const names = require("./names")
-console.log(hobbies)
 
 function schedule(){
     return{
-    names,
-    hobbies
+    names : names('Enrico', 'Ciccolini'),
+    hobbies : hobbies('mangiare', 'studiare', 'dormire')
     }
 }
 
